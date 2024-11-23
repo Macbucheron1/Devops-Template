62f283df0e9452a14e6786f4b456fbafc36cca2b
mac <nathandeprat@hotmail.fr>
2024-11-23 23:59:10 +0100

    fix: fixed the lint error

--- user_api/src/dbClient.js
+++ user_api/src/dbClient.js
@@ -5,30 +5,17 @@
  */
 
 var redis = require("redis");
-const configure = require("./configure");
 require("dotenv").config({ path: ".env.local" });
 
 // Use environment variables for Redis configuration
 const REDIS_PORT = process.env.REDIS_PORT || 6379;
-const REDIS_HOST = process.env.REDIS_HOST || '127.0.0.1';
+const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
 const REDIS_PASSWORD = process.env.REDIS_PASSWORD || null;
-const REDIS_SSL = process.env.REDIS_SSL === 'true';
-
-console.log(`REDIS_HOST: ${REDIS_HOST}`);
-console.log(`REDIS_PORT: ${REDIS_PORT}`);
-console.log(`REDIS_PASSWORD: ${REDIS_PASSWORD}`);
 
 if (!REDIS_HOST || !REDIS_PORT || !REDIS_PASSWORD) {
   throw new Error("Missing Redis configuration");
 }
 
-
-/**
- * Retrieves configuration settings.
- *
- * @type {Object}
- */
-const config = configure();
 
 /**
  * Creates a Redis client with specified host, port, and retry strategy.
@@ -39,7 +26,7 @@ var db = redis.createClient({
   host: REDIS_HOST,
   port: REDIS_PORT,
   password: REDIS_PASSWORD,
-  tls: { servername: REDIS_HOST},
+  tls: { servername: REDIS_HOST },
   /**
    * Custom retry strategy for the Redis client.
    * Returns an error when retry time is exhausted.
